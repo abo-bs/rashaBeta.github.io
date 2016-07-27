986a5235bc2a960853ff7960021464ada0bde5e5
Abo-bs <xss.5@hotmail.com>
2016-07-27 23:44:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+(function() {
+	"use strict";
+
+	window.onload = function() { document.body.className = ''; }
+	window.ontouchmove = function() { return false; }
+	window.onorientationchange = function() { document.body.scrollTop = 0; }
+})();
